f04eaec0addad6f8961dd0ca3920ab952b3dcd66
Vijayaraj <rajanvijaya080@gmail.com>
2025-04-03 16:37:50 +0530

    refferal code working

--- utils/googleAuth.js
+++ utils/googleAuth.js
@@ -4,7 +4,7 @@ import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://virtuerider.shop/auth/google/callback"
+    callbackURL: "http://virtuerider.shop/auth/google/callback"
   },
   async (token, tokenSecret, profile, done) => {
     try {
